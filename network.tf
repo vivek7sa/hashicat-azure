5023cf25d4144778cf6b21ab66622fcd3bc85b30
vivek saxena <Vivek7sa@gmail.com>
2022-08-02 21:45:11 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,6 @@
 module "network" {
   source  = "app.terraform.io/vivekhashicorp123/network/azurerm"
   version = "1.0.0"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
 }
